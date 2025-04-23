0d96d8ac04a0d029b4d66569a05eb3a22c1a5da8
J C Lisboa <lookjota@gmail.com>
2025-04-23 16:23:25 -0300

    update dependencies

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -198,7 +198,7 @@ const HomePage = () => {
 									<svg className="w-5 h-5 text-green-600 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 										<path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
 									</svg>
-									<span className="ml-2 italic">Notebook's</span>
+									<span className="ml-2 italic">Notebooks</span>
 								</li>
 								<li className="flex items-center">
 									<svg className="w-5 h-5 text-green-600 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
